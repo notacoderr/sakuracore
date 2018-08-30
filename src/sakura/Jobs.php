bf44c0471b5c9e010cbd373155530dd2441289bd
notacoderr <38270548+notacoderr@users.noreply.github.com>
2018-08-31 07:32:30 +0800

    Update Jobs.php

--- src/sakura/Jobs.php
+++ src/sakura/Jobs.php
@@ -9,12 +9,27 @@ use pocketmine\Server;
 
 class Jobs
 {
-  
-  public $main;
-  
+
+	public $main;
+
 	public function __construct(core $core)
-  {
-        $this->main = $core;
-  }
-  
+	{
+        	$this->main = $core;
+	}
+
+	public function getJob(Player $player) : string
+	{
+		$result = $this->main->db->query("SELECT * FROM jobs WHERE name = '$player->getName()';");
+		$resultArr = $result->fetchArray(SQLITE3_ASSOC);
+		return $resultArr[ "job" ];
+	}
+ 
+ 	public function setJob(Player $player, string $newjob) : void
+	{
+		$stmt = $this->main->db->prepare("INSERT OR REPLACE INTO jobs (name, job) VALUES (:name, :job);");
+		$stmt->bindValue(":name", $player->getName());
+		$stmt->bindValue(":job", $newjob);
+		$result = $stmt->execute();
+    	}
+	
 }
