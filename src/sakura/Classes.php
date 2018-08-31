c02297a5ae52c1381c3767bd79b14c0f82dcd3d5
notacoderr <38270548+notacoderr@users.noreply.github.com>
2018-08-31 23:23:31 +0800

    Update Classes.php

--- src/sakura/Classes.php
+++ src/sakura/Classes.php
@@ -34,4 +34,37 @@ class Classes
 		$result = $stmt->execute();
     	}
 	
+	public function mainForm(Player $player)
+    	{
+		$form = $this->main->formapi->createSimpleForm(function (Player $player, array $data)
+		{
+			if (isset($data[0])){
+			$button = $data[0];
+
+				switch ($button)
+				{
+					case 0: break;
+					case 1:	break;
+					case 2:	break;
+					case 3:	break;
+				}
+
+				return true;
+			}
+		});
+        	$form->setTitle('§l§fClass Selection');
+		
+		$form->addButton('§lWarrior'); //0
+		$form->addButton('§lPaladin'); //1
+		$form->addButton('§lAssassin'); //2
+		$form->addButton('§lRanger'); //3
+		/*$form->addButton('§lKid'); //4
+	    	$form->addButton('§lTeen'); //5
+	    	$form->addButton('§lNormal'); //6
+		$form->addButton('§lGiant§r [VIP & up]'); //7
+	    	$form->addButton('§lTera§r [VIP & up]'); //8
+		$form->addButton('§lTitan§r [VIP & up]'); //9*/
+		
+        	$form->sendToPlayer($player);
+    	}
 }
