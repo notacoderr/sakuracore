e8614b495af98759c7dfce5003e8b13d9867346b
notacoderr <38270548+notacoderr@users.noreply.github.com>
2018-09-01 13:08:32 +0800

    Update Datas.php

--- src/sakura/Datas.php
+++ src/sakura/Datas.php
@@ -47,38 +47,39 @@ class Datas
 		}
 	}
   
- 	public function addVal(Player $player, string $val, int $add)
+ 	public function addVal(Player $player, string $val, int $add) : void
 	{
     		$name = $player->getName();
 		switch ($val)
 		{
 		      case 'level':
-			//$f = $this->getVal($name, "level") + 1;
-			  $stmt = $this->main->db->prepare("INSERT OR REPLACE INTO lvl (name, level) VALUES (:name, :level);");
-			    $stmt->bindValue(":name", $name);
-			      $stmt->bindValue(":level", $add);
-				$result = $stmt->execute();
+			$stmt = $this->main->db->prepare("INSERT OR REPLACE INTO lvl (name, level) VALUES (:name, :level);");
+			$stmt->bindValue(":name", $name);
+			$stmt->bindValue(":level", $add);
+			$result = $stmt->execute();
 		      break;
+				
 		      case 'exp':
 			$f = $this->getVal($player, "exp") + $add;
-			  $stmt = $this->main->db->prepare("INSERT OR REPLACE INTO exp (name, exp) VALUES (:name, :exp);");
-			    $stmt->bindValue(":name", $name);
-			      $stmt->bindValue(":exp", $f);
-				$result = $stmt->execute();
-				  $this->testLevel($name, $f);
-				    $this->Alert($name, 1, $add);
+			$stmt = $this->main->db->prepare("INSERT OR REPLACE INTO exp (name, exp) VALUES (:name, :exp);");
+			$stmt->bindValue(":name", $name);
+			$stmt->bindValue(":exp", $f);
+			$result = $stmt->execute();
+			$this->testLevel($name, $f);
+			$player->sendPopup("§l§a +" . $add . " experience");
 		      break;
+				
 		      case 'gems':
 			$f = $this->getVal($player, "gems") + $add;
-			  $stmt = $this->main->db->prepare("INSERT OR REPLACE INTO gem (name, gems) VALUES (:name, :gems);");
-			    $stmt->bindValue(":name", $name);
-			      $stmt->bindValue(":gems", $f );
-				$result = $stmt->execute();
-				  /*if ($add > 0){ 
-				    return $this->Alert($name, 7, $add);
-				  } else { 
-				    return $this->Alert($name, 8, $add);
-				  }*/
+			$stmt = $this->main->db->prepare("INSERT OR REPLACE INTO gem (name, gems) VALUES (:name, :gems);");
+			$stmt->bindValue(":name", $name);
+			$stmt->bindValue(":gems", $f );
+			$result = $stmt->execute();
+			if ($add > 0){ 
+				$player->sendMessage("§l§7>§a $add was added to your account.");
+			} else { 
+			    	$player->sendMessage("§l§7>§c $add was deducted from your account.");
+			}
 		      break;
 		}
     	}
