8b6805fb00b756055dbce5ffc8ebc8cc2b9b4dd7
notacoderr <38270548+notacoderr@users.noreply.github.com>
2018-12-20 12:54:04 +0800

    Create Titles.php
    INITIAL COMMIT

---
+++ src/sakura/Titles.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace sakura;
+
+use sakura\core;
+
+use pocketmine\Player;
+use pocketmine\Server;
+
+class Titles
+{
+
+	public $main;
+
+	public function __construct(core $core)
+	{
+        	$this->main = $core;
+	}
+
+	public function getTitle(Player $player) : string
+	{
+		$name = $player->getName();
+		$result = $this->main->db->query("SELECT inuse FROM titles WHERE name = '$name';");
+		$resultArr = $result->fetchArray(SQLITE3_ASSOC);
+		return $resultArr["inuse"];
+	}
+  
+	public function getAllTitles(Player $player) : string
+	{
+		$name = $player->getName();
+		$result = $this->main->db->query("SELECT titles FROM titles WHERE name = '$name';");
+		$resultArr = $result->fetchArray(SQLITE3_ASSOC);
+		return $resultArr["titles"];
+	}
+ 
+ 	public function useTitle(Player $player, string $title) : void
+	{
+		$name = $player->getName();
+    $titles = $this->trimTitles($player, $title);
+		$stmt = $this->main->db->prepare("INSERT OR REPLACE INTO titles (name, titles, inuse) VALUES (:name, :titles, :inuse);");
+		$stmt->bindValue(":name", $name);
+    $stmt->bindValue(":titles", $titles);
+		$stmt->bindValue(":inuse", $title);
+		$result = $stmt->execute();
+  }
+
+	public function trimTitles(Player $player, string $x) : string
+	{
+		$titles = explode("@", $this->getAllTitles($player) );
+    unset( $titles[$x] );
+    $titles = implode("@", $titles);
+		return $titles;
+	}
+  
+ public function addTitle(Player $player, string $x) : string
+	{
+    $name = $player->getName();
+		$titles = (string) $this->getAllTitles($player). "@". $x;
+    
+   	$stmt = $this->main->db->prepare("INSERT OR REPLACE INTO titles (name, titles, inuse) VALUES (:name, :titles, :inuse);");
+		$stmt->bindValue(":name", $name);
+    $stmt->bindValue(":titles", $titles);
+		$stmt->bindValue(":inuse", $this->getTitle($player));
+		$result = $stmt->execute();
+	}
+  
+	public function sendForm(Player $player)
+  {
+		$form = $this->main->formapi->createSimpleForm(function (Player $player, array $data)
+		{
+			if (isset($data[0])){
+			$button = $data[0];
+
+				switch ($button)
+				{
+					case 0: break;
+					case 1:	break;
+					case 2:	break;
+					case 3:	break;
+				}
+
+				return true;
+			}
+		});
+    $form->setTitle('§l§fTitle Picker');
+		//buttons here | TO DO
+    $form->sendToPlayer($player);
+    }
+}
