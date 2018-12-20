b7a84f10130f2e241b2258430a0a320287cde93d
notacoderr <38270548+notacoderr@users.noreply.github.com>
2018-12-20 13:42:12 +0800

    Create calculateExp.php

---
+++ src/sakura/calculateExp.php
@@ -0,0 +1,58 @@
+<?php
+namespace sakura;
+
+use sakura\core;
+
+use pocketmine\Player;
+use pocketmine\Server;
+
+class calculateExp
+{
+
+	public $main;
+  
+	public function __construct(core $core)
+	{
+        	$this->main = $core;
+	}
+
+	function doMagic(Player $player, $experience) : void
+	{
+  
+		$base = (int) $this->main->settings->get("baseExp");
+		$plevel = (int) $this->main->data->getVal($player, "level");
+    $multi = (float) ($this->main->data->getVal($player, "multiplier") / 100);
+    $bonus = (int) ($experience * $multi)
+    $experience = (int) ($experience + $bonus);
+    
+		$goal = $base * $plevel;
+		if ($experience >= $goal)
+		{
+			$extra = $experience - $goal;
+			$Ngoal = $goal + $base;
+			$i = 0;
+      
+			do
+			{
+				$i += 1;
+				if ($extra >= $Ngoal)
+				{
+					$extra = $extra - $Ngoal;
+				}
+			}
+			while ($extra >= $Ngoal);
+      
+			$f = $plevel + $i;
+			$this->main->data->addVal($player, "level", $plevel + $i);
+      
+			$player->addTitle("§l§fLevel UP §7[§6 $f §7]", "§fNext Level on §7[§f $extra §7/§d $Ngoal §7");
+      
+			$stmt = $this->main->db->prepare("INSERT OR REPLACE INTO exp (name, exp) VALUES (:name, :exp);");
+			$stmt->bindValue(":name", $player->getName() );
+			$stmt->bindValue(":exp", $extra);
+			$result = $stmt->execute();
+      
+		}
+	}
+
+}
