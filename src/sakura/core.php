a1a7f2c78fe78cece4135a3cdc3d62ef440483d7
notacoderr <38270548+notacoderr@users.noreply.github.com>
2019-01-21 17:42:03 +0800

    Update core.php

--- src/sakura/core.php
+++ src/sakura/core.php
@@ -1,279 +1,124 @@
 <?php
 
-
-
 namespace sakura;
 
-
-
 use pocketmine\Server;
-
 use pocketmine\Player;
-
-
-
 use pocketmine\plugin\PluginBase;
-
-
-
 use pocketmine\command\Command;
-
 use pocketmine\command\CommandSender;
-
 use pocketmine\command\ConsoleCommandSender;
-
-
-
 use pocketmine\event\Listener;
-
 #use pocketmine\event\player\PlayerJoinEvent;
-
 use pocketmine\event\player\PlayerLoginEvent;
-
 use pocketmine\utils\Config;
-
 use pocketmine\utils\TextFormat;
-
-
-
 use pocketmine\level\level;
-
-
-
 use pocketmine\item\Item;
-
-use pocketmine\inventory\Inventory;
-
-use pocketmine\item\enchantment\{Enchantment, EnchantmentInstance};
-
-
+use pocketmine\inventory\Inventory;use pocketmine\item\enchantment\{Enchantment, EnchantmentInstance};
 
 class core extends PluginBase implements Listener {
-
-
-
+	
 	public $db;
-
-
-
 	public function onEnable() : void
-
 	{	
-
 		$this->saveResource('settings.yml');
-
 		$this->saveResource('quests.yml');
-
 		$this->saveResource('items.yml');
-
 		$this->saveResource('recipe.yml');
-
-		
-
 		$this->settings = new Config($this->getDataFolder() . "settings.yml", CONFIG::YAML);
-
 		$this->questData = new Config($this->getDataFolder() . "quests.yml", CONFIG::YAML);
-
 		$this->itemData = new Config($this->getDataFolder() . "items.yml", CONFIG::YAML);
-
 		$this->recipeData = new Config($this->getDataFolder() . "recipe.yml", CONFIG::YAML);
-
-		
-
 		$this->db = new \SQLite3($this->getDataFolder() . "coredrive-v2.db"); //creating main database
-
-		
-
 		$this->db->exec("CREATE TABLE IF NOT EXISTS gem (name TEXT PRIMARY KEY COLLATE NOCASE, gems INT);");
-
 		$this->db->exec("CREATE TABLE IF NOT EXISTS exp (name TEXT PRIMARY KEY COLLATE NOCASE, exp INT, multiplier INT);");
-
 		$this->db->exec("CREATE TABLE IF NOT EXISTS lvl (name TEXT PRIMARY KEY COLLATE NOCASE, level INT);");
-
-		
-
 		$this->db->exec("CREATE TABLE IF NOT EXISTS titles (name TEXT PRIMARY KEY COLLATE NOCASE, titles BLOB, inuse BLOB);");
-
-		
-
 		$this->db->exec("CREATE TABLE IF NOT EXISTS elo (name TEXT PRIMARY KEY COLLATE NOCASE, rank TEXT, div INT, points INT);");
-
-		
-
 		//$this->db->exec("CREATE TABLE IF NOT EXISTS guild (guild TEXT PRIMARY KEY COLLATE NOCASE, founder INT);");
-
 		//$this->db->exec("CREATE TABLE IF NOT EXISTS member (name TEXT PRIMARY KEY COLLATE NOCASE, guild INT);");
-
-		
-
 		$this->db->exec("CREATE TABLE IF NOT EXISTS pquests (name TEXT PRIMARY KEY COLLATE NOCASE, quest TEXT);");
-
 		$this->db->exec("CREATE TABLE IF NOT EXISTS pcompleted (name TEXT PRIMARY KEY COLLATE NOCASE, quests TEXT);");
-
-		
-
 		$this->db->exec("CREATE TABLE IF NOT EXISTS classes (name TEXT PRIMARY KEY COLLATE NOCASE, class INT);");
-
 		$this->db->exec("CREATE TABLE IF NOT EXISTS vault (name TEXT PRIMARY KEY COLLATE NOCASE, items BLOB, max INT, code TEXT, owner TEXT);");
-
 		try{
-
 			$this->db->exec("ALTER TABLE vault ADD COLUMN code TEXT default null");
-
 			$this->db->exec("ALTER TABLE vault ADD COLUMN owner TEXT default null");
-
 			$this->db->exec("ALTER TABLE exp ADD COLUMN multiplier INT default 0");
-
 		    	$this->getLogger()->info("Wudafak");
-
-		}catch(\ErrorException $ex){
-
-		}
-
-		
+		}catch(\ErrorException $ex){ }
 
 		$this->calculate = new calculateExp($this); //Calcu handler
-
 		$this->classes = new Classes($this); //Class Handler
-
 		$this->quests = new Quests($this); //Quests Handler
-
 		$this->titles = new Titles($this); //Titles Handler
-
 		$this->vault = new Vault($this); //Vault Handler
-
 		$this->items = new Items($this); //Item Handler
-
 		$this->data = new Datas($this); //Data Value Handler
-
 		$this->elo = new Elo($this); //Elo Handler
 
-		
-
 		if(Server::getInstance()->getPluginManager()->getPlugin("PiggyCustomEnchants") !== null ){
-
 			$this->pce = Server::getInstance()->getPluginManager()->getPlugin("PiggyCustomEnchants");
-
 		}
-
 		if(Server::getInstance()->getPluginManager()->getPlugin("EconomyAPI") !== null ){
-
 			$this->eco = Server::getInstance()->getPluginManager()->getPlugin("EconomyAPI");
-
 		}
-
 		$this->formapi = Server::getInstance()->getPluginManager()->getPlugin("FormAPI");
-
-		
-
 		Server::getInstance()->getPluginManager()->registerEvents($this, $this);
-
-		
-
 	}
-
 	
-
 	public function onCommand(CommandSender $sender, Command $command, $label, array $args) : bool 
-
 	{	
-
 		switch (strtolower( $command->getName() ))
-
 		{
-
 			case 'grant':
-
 				if ($sender instanceof Player)
-
 				{
-
 					$sender->sendMessage("§cCan only be used it §f§lCONSOLE");
-
 					return true;
-
 				}
-
 				if (count($args) <> 3)
-
 				{
-
 					$sender->sendMessage("Invalid usage, /grant <playername> <exp/gems/pts> <amount>");
-
 					return true;
-
 				}
-
 				if(isset($args[0])){  //realc
-
 					$target = $this->getServer()->getPlayer($args[0]);
-
 					if(!$target instanceof Player)
-
 					{
-
 						$sender->sendMessage("Player is not online");
-
 						return true;
-
 					}
-
 				}
-
 				if(!$this->isRecorded($target)){
-
 					  $sender->sendMessage("No Record found for $target");
-
 					  return true;
-
 				}
-
 				if (!is_numeric($args[2]) )
-
 				{
-
 					$sender->sendMessage("must be an integer");
-
 					return true;
-
 				}
-
 				switch ( strtolower($args[1]) )
-
 				{
-
 					case "exp": case "xp": case "e":
-
 						$this->calculate->doMagic($target, $args[2]);
-
 					break;
-
 					case "gems": case "gem": case "g":
-
 						$this->data->addVal($target, "gems", $args[2]);
-
 					break;
-
 					case "pts": case "elo": case "p":
-
 						$this->elo->increasePoints($target, (int) $args[2]);
-
 					break;
-
-						
 
 					default: $sender->sendMessage("invalid command"); return true;
 
 				}
-
 				$sender->sendMessage("added $args[2] of $args[1] to ".$target->getName());
-
 			break;
 
-			
-
 			case "takegem":
-
 				if ($sender instanceof Player)
 				{
 					$sender->sendMessage("§cCan only be used it §f§lCONSOLE");
@@ -519,14 +364,14 @@ class core extends PluginBase implements Listener {
 					break;*/
 				}
 			break;
-			case "+":
+			case "+i":
 				/**
 				*Args 0 = player name
 				*Args 1 = item serial
 				**/
 				if (count($args) <> 2)
 				{
-					$sender->sendMessage("Invalid usage, /+ <playername> <serial>");
+					$sender->sendMessage("Invalid usage, /+i <playername> <serial>");
 					return true;
 				}
 				if(!array_key_exists($args[1], $this->itemData->getAll()))
@@ -539,7 +384,7 @@ class core extends PluginBase implements Listener {
 					$sender->sendMessage("Failed to produce item: ". $args[1]. ", Player is not available");
 					return true;
 				}
-				$this->items->produceItem($args[1]);
+				$this->items->produceItem($target, $args[1]);
 				/*if($this->hasSpace($target) == false)
 				{
 					$target->sendMessage("No slot available");
@@ -551,83 +396,55 @@ class core extends PluginBase implements Listener {
 			break;
 		}
 		return true;
-
 	}
-
-	
 
 	public function onLogIn(PlayerLoginEvent $event) : void
-
 	{
-
 		if (!$this->isRecorded( $event->getPlayer() ))
-
 		{
-
 			$this->register( $event->getPlayer() );
-
 		}
-
 	}
-
-	
 
 	public function hasSpace(Player $player) : bool
-
 	{
-
 		return $player->getInventory()->canAddItem(Item::get(Item::STICK, 0, 1)) ? true : false; //Test item xD
-
 	}
 
-	
-
 	public function register(Player $player) : void
-
 	{
-
 		$stmt = $this->db->prepare("INSERT OR REPLACE INTO exp (name, exp, multiplier) VALUES (:name, :exp, :multiplier);");
-
 		$stmt->bindValue(":name", $player->getName() );
 		$stmt->bindValue(":exp", 0);
 		$stmt->bindValue(":multiplier", 0);
-
 		$result = $stmt->execute();
-
 		
-
 		$stmt = $this->db->prepare("INSERT OR REPLACE INTO gem (name, gems) VALUES (:name, :gems);");
-
 		$stmt->bindValue(":name", $player->getName() );
-
 		$stmt->bindValue(":gems", 0);
-
 		$result = $stmt->execute();
-
 		
-
 		$stmt = $this->db->prepare("INSERT OR REPLACE INTO lvl (name, level) VALUES (:name, :level);");
-
 		$stmt->bindValue(":name", $player->getName() );
-
 		$stmt->bindValue(":level", 1);
-
 		$result = $stmt->execute();
-
+		
 		$stmt = $this->db->prepare("INSERT OR REPLACE INTO elo (name, rank, div, points) VALUES (:name, :rank, :div, :points);");
 		$stmt->bindValue(":name", $player->getName() );
 		$stmt->bindValue(":rank", "Iron");
 		$stmt->bindValue(":div", 5);
 		$stmt->bindValue(":points", 0);
 		$result = $stmt->execute();
+		
 		$stmt = $this->db->prepare("INSERT OR REPLACE INTO classes (name, class) VALUES (:name, :class);");
 		$stmt->bindValue(":name", $player->getName() );
 		$stmt->bindValue(":class", "Recruit");
 		$result = $stmt->execute();
+		
 		$stmt = $this->db->prepare("INSERT OR REPLACE INTO titles (name, titles, inuse) VALUES (:name, :titles, :inuse);");
 		$stmt->bindValue(":name", $player->getName() );
 		$stmt->bindValue(":titles", "§7§lI Love Sakura");
-		$stmt->bindValue(":inuse", "§7§l_Rookie_");
+		$stmt->bindValue(":inuse", "§7§l_Beginner_");
 		$result = $stmt->execute();
 	}
 
