6910391ee20a52ed706aefdc2e53749fe6e56d9e
notacoderr <38270548+notacoderr@users.noreply.github.com>
2019-01-21 17:34:12 +0800

    Update Items.php

--- src/sakura/Items.php
+++ src/sakura/Items.php
@@ -3,34 +3,33 @@
 namespace sakura;
 
 use sakura\core;
-use pocketmine\Player;
+use pocketmine\{Player, Server};
 use pocketmine\item\Item;
-use pocketmine\item\enchantment\Enchantment;
-use pocketmine\item\enchantment\EnchantmentInstance;
+use pocketmine\math\Vector3;
+use pocketmine\item\enchantment\{Enchantment, EnchantmentInstance};
 use pocketmine\inventory\Inventory;
 
 use pocketmine\utils\TextFormat as TF;
 
 class Items
 {
-  
     public $main;
-	
     public function __construct(core $core)
     {
         $this->main = $core;
     }
-  
     public function isCompatible(Player $player, Item $item) : bool
     {
+	 if($player->hasPermission("corex.item.master")
+	 {
+		 return true;
+	 }
          $playerClass = $this->main->classes->getClass($player);
          $playerLevel = $this->main->data->getVal($player, "level");
-      
          $itemLore = $item->getlore();
       	 $itemLevel = (int) TF::clean($itemLore[0]); //Lore 0
 	 $itemClass = (string) TF::clean($itemLore[1]); //Lore 1 (array)
 	 $itemSerial = (string) TF::clean($itemLore[1]); //Lore 3 (array)
-	    
 	if(array_key_exists( $itemSerial, $this->main->itemData->getAll() ))
 	{	
 		if($playerLevel < $itemLevel)
@@ -38,38 +37,36 @@ class Items
 			$player->sendMessage("§c§lYour Level is too low");
 			return false;
 		}
-		
 		if($itemClass == "General")
 		{
 			  return true;
 		} else {
-			  if(strpos($itemClass, $playerClass) !== false)
+			  if($itemClass != $playerClass)
 			  {
 				return true;
 			  } else {
-				$player->sendMessage("§c§lThis weapon is not for your Class type");
+				$player->sendMessage("§c§lThe weapon was made for §f". $itemClass);
 				return false;
 			  }
 		 }
 	}
-	    
-	  
-          //return (strtolower($itemClass) == strtolower($playerClass)) ? true : false;
+        //return (strtolower($itemClass) == strtolower($playerClass)) ? true : false;
     }
-  
+	
+    public function produceItem(Player $player, string $data) : void
+    {
+	    $player->getLevel()->dropItem(new Vector3($player->getX(), $player->getY() + 0.75, $player->getZ()), $this->createItem($data));
+    }
+	
     public function createItem(string $data) : Item
     {  
           	$src = $this->main->itemData;
-	    
 		$item = Item::get($src->getNested($data .".item"), 0 ,1); 
-	    
 		$class = $src->getNested($data .".class");
 		$level = $src->getNested($data .".level");
 		$rarity = $src->getNested($data .".rare");
 		$info = $src->getNested($data .".info");
-	    
 		$item->setCustomName($src->getNested($data .".name"));
-	    
 	  	$item->setLore([
 			"Required Lv: ". TF::RED. $level, //Level #0
 			"Class: ". TF::GOLD. $class, //Class #1
@@ -77,7 +74,6 @@ class Items
 			"Serial: ". TF::WHITE. $data, //Level #3
 			"- ". TF::WHITE. $info //#4
 		]);
-	    
 	  	if(!is_null($src->getNested($data .".enchantment")))
 	  	{
 			foreach($src->getNested($data .".enchantment") as $enc)
@@ -97,7 +93,6 @@ class Items
 				}
 		  	}
 	  	}
-	    
           	return $item;
     }
   
