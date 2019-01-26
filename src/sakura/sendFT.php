450a6b5a1af094dc65c3e24e4fd0cafa61936eee
notacoderr <38270548+notacoderr@users.noreply.github.com>
2019-01-26 19:03:57 +0800

    Update sendFT.php

--- src/sakura/sendFT.php
+++ src/sakura/sendFT.php
@@ -2,34 +2,38 @@
 
 namespace sakura;
 
+use sakura\core;
+use pocketmine\Server;
+use pocketmine\math\Vector3;
 use pocketmine\scheduler\Task;
+use pocketmine\level\particle\FloatingTextParticle;
 
 class sendFT extends Task
 {
   
   private $core, $worlds;
   
-	public function __construct(Newspaper $core, array $worlds)
-  {
-	  $this->core = $core;
-    $this->worlds = $worlds;
+	public function __construct(core $core, array $worlds)
+  	{
+		$this->core = $core;
+    		$this->worlds = $worlds;
 	}
   
 	public function onRun(int $currentTick)
-  {
+  	{
 		foreach($this->worlds as $world => $coords)
-    {
-      $xyzv = explode(":", $coords);
-      $v3 = new Vector3((float) $xyzv[0],(float) $xyzv[1],(float) $xyzv[2]);
-      $type = $xyzv[3];
-      $this->addText($v3, $type, $world);
-    } 
+    		{
+		      $xyzv = explode(":", $coords);
+		      $v3 = new Vector3((float) $xyzv[0],(float) $xyzv[1],(float) $xyzv[2]);
+		      $type = $xyzv[3];
+		      $this->addText($v3, $type, $world);
+    		} 
 	}
   
-  //$world = [];
-  private function addText(Vector3 $location, string $type, string $world, $player = null) : void
-  {
-    switch ($this->getServer()->getName()) {
+	private function addText(Vector3 $location, string $type, string $world, $player = null) : void
+	{
+   		switch (Server::getInstance()->getName())
+		{
 			case 'PocketMine-MP':
 				$particle = new FloatingTextParticle($location, color::GOLD . "<<<<<>>>>>", $this->getType($type));
 				$this->getServer()->getLevelByName($world)->addParticle($particle, $player);
@@ -39,17 +43,17 @@ class sendFT extends Task
 				$this->getServer()->getLevelByName($world)->addParticle($location, $particle);
 				break;
 		}
-  }
-  
-  private getType(string $type) : string
-  {
-    switch($type)
-    {
-      case "elo":
-        return "test";
-      break;
-      default:
-        return "test";
-    }
-  }
+  	}
+  	
+	private getType(string $type) : string
+  	{
+    		switch($type)
+    		{
+      			case "elo":
+        			return "test";
+      			break;
+      			default:
+        			return "test";
+    		}
+  	}
 }
