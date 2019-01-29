fda7d13a2e6d13d58a698999d9f587b3f83a7f66
notacoderr <38270548+notacoderr@users.noreply.github.com>
2019-01-30 01:34:37 +0800

    Update RefreshSigns.php

--- src/sakura/tasks/RefreshSigns.php
+++ src/sakura/tasks/RefreshSigns.php
@@ -1,1 +1,30 @@
+<?php
 
+namespace sakura\tasks;
+
+use sakura\core;
+use pocketmine\Server;
+use pocketmine\level\Location;
+use pocketmine\level\Position;
+use pocketmine\tile\Sign;
+use pocketmine\scheduler\Task;
+
+use pocketmine\utils\TextFormat;
+
+class ProcessSigns extends Task
+{
+  
+  	private $core, $topLevelSigns = [], $topEloSigns = [];
+  
+	public function __construct(core $core)
+	{
+		$this->core = $core;
+	}
+	
+  public function onRun(int $tick)
+	{
+		$this->reloadSigns();
+	}
+  public function onSuccess(): void { $this->core->getScheduler()->cancelTask($this->getTaskId()); }
+	
+}
